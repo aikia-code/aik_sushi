0d1250b59bed707d9bd13e306747770e09e451f6
Isaac <prosperokay@gmail.com>
2024-11-04 13:58:34 +0000

    refactor: modify app name

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,11 @@ import 'package:aik_sushi/screens/home/home.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const AikSushi());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class AikSushi extends StatelessWidget {
+  const AikSushi({super.key});
 
   @override
   Widget build(BuildContext context) {
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:aik_sushi/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const AikSushi());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
