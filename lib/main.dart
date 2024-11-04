7162bbf59cd1e9e793b152d1fc1dbd5fa96d0b6d
Isaac <prosperokay@gmail.com>
2024-11-04 16:38:23 +0000

    refactor: make app stateful

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,14 @@ void main() {
   runApp(const AikSushi());
 }
 
-class AikSushi extends StatelessWidget {
+class AikSushi extends StatefulWidget {
   const AikSushi({super.key});
 
+  @override
+  State<AikSushi> createState() => _AikSushiState();
+}
+
+class _AikSushiState extends State<AikSushi> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
